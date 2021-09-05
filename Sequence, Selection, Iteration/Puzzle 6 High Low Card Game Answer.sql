8c82b245d702cd24d3ab9b0b0ceba847168d3f5d
smpetersgithub <56611711+smpetersgithub@users.noreply.github.com>
2021-09-04 21:02:33 -0500

    Update Puzzle 6 High Low Card Game Answer.sql

--- Sequence, Selection, Iteration/Puzzle 6 High Low Card Game Answer.sql
+++ Sequence, Selection, Iteration/Puzzle 6 High Low Card Game Answer.sql
@@ -1,7 +1,7 @@
 /*********************************************************************
 Answer to Puzzle #6
 High Low Card Game
-https://AdvancedSQLPuzzles.com
+https://advancedsqlpuzzles.com
 
 Developer Notes:
 
