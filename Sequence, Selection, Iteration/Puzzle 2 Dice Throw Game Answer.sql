ecd5643f56d19509f6f00ca11e9751d8ef12f251
smpetersgithub <56611711+smpetersgithub@users.noreply.github.com>
2021-09-04 21:01:16 -0500

    Update Puzzle 2 Dice Throw Game Answer.sql

--- Sequence, Selection, Iteration/Puzzle 2 Dice Throw Game Answer.sql
+++ Sequence, Selection, Iteration/Puzzle 2 Dice Throw Game Answer.sql
@@ -1,7 +1,7 @@
 /*********************************************************************
 Answer to Puzzle #2
 Dice Throw Game
-https://AdvancedSQLPuzzles.com
+https://advancedsqlpuzzles.com
 
 Modify the following variable to change the number of trials
 @vNumberOfTrials
