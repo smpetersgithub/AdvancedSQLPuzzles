087eee2cc6ce1ccb3c5d108dd83a4b3d5c207843
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2021-09-11 15:46:42 -0500

    Update Advanced SQL Puzzles Solutions.sql

--- Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
+++ Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
@@ -1322,9 +1322,20 @@ Answer to Puzzle #28
 Fill the Gaps
 */----------------------------------------------------
 
+/*
+Here is an excellent post about this problem.
+https://www.red-gate.com/simple-talk/sql/t-sql-programming/filling-in-missing-values-using-the-t-sql-window-frame/
+*/
+
+
 IF OBJECT_ID('tempdb.dbo.#Gaps','U') IS NOT NULL
 	DROP TABLE #Gaps;
 GO
+
+IF OBJECT_ID('tempdb.dbo.#Gaps2','U') IS NOT NULL
+	DROP TABLE #Gaps2;
+GO
+
 
 CREATE TABLE #Gaps
 (
@@ -1345,32 +1356,43 @@ INSERT INTO #Gaps VALUES
 (9,NULL);
 GO
 
+SELECT * INTO #Gaps2 FROM #Gaps;
+GO
+
 --Solution 1
-SELECT	c.RowNumber,
-		(SELECT	 d.TestCase 
-		 FROM	 #Gaps d 
-		 WHERE	 d. RowNumber = 
-					 (SELECT MAX(e. RowNumber) 
-					 FROM #Gaps e 
-					 WHERE e. RowNumber <= c. RowNumber AND e.TestCase != '')) TestCase
-FROM #Gaps c;
+SELECT	a.RowNumber,
+		(SELECT	 b.TestCase
+		 FROM	 #Gaps b
+		 WHERE	 b.RowNumber =
+					 (SELECT MAX(c.RowNumber)
+					 FROM #Gaps c
+					 WHERE c.RowNumber <= a.RowNumber AND c.TestCase != '')) TestCase
+FROM #Gaps a;
+
 
 --Solution 2
-BEGIN
-	DECLARE @v VARCHAR(MAX);
-	UPDATE #Gaps WITH(TABLOCKX)
-	SET @v = TestCase = CASE WHEN TestCase IS NULL THEN @v ELSE TestCase END
-	OPTION(MAXDOP 1);
-	SELECT RowNumber, TestCase FROM #Gaps;
-END
-
---Solution 3
-SELECT	RowNumber, MAX(TestCase) OVER (PARTITION BY DistinctCount) AS TestCase
-FROM	(SELECT	RowNumber, 
+SELECT	RowNumber,
+		MAX(TestCase) OVER (PARTITION BY DistinctCount) AS TestCase
+FROM	(SELECT	RowNumber,
 				TestCase,
 				COUNT(TestCase) OVER (ORDER BY RowNumber) AS DistinctCount
 		FROM #Gaps) a
 ORDER BY RowNumber;
+
+--Solution 3
+--This type of update is called a "quirky update"
+--https://ask.sqlservercentral.com/questions/5150/please-can-somebody-explain-how-quirky-updates-wor.html
+--There is no guarantee that this UPDATE will always produce the correct result.  You must have another 
+--method to validate the results.
+BEGIN
+	DECLARE @v VARCHAR(MAX);
+	UPDATE #Gaps2 WITH(TABLOCKX)
+	SET @v = TestCase = CASE WHEN TestCase IS NULL THEN @v ELSE TestCase END
+	SELECT RowNumber, TestCase FROM #Gaps;
+END
+
+SELECT * FROM #Gaps2;
+GO
 
 
 /*----------------------------------------------------
