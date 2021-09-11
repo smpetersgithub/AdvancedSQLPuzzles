37897e80853dd1e17ff9ec38913672aee6ce5ea0
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2021-09-11 17:30:55 -0500

    Update Advanced SQL Puzzles Solutions.sql

--- Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
+++ Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
@@ -907,19 +907,26 @@ Answer to Puzzle #19
 Back to the Future
 */----------------------------------------------------
 
-IF OBJECT_ID('tempdb.dbo.#TimePeriods','U') IS NOT NULL
-  DROP TABLE #TimePeriods;
+/*----------------------------------------------------
+Answer to Puzzle #19
+Back to the Future
+*/----------------------------------------------------
+
+IF OBJECT_ID('tempdb.dbo.##TimePeriods','U') IS NOT NULL
+  DROP TABLE ##TimePeriods;
 GO
 
-CREATE TABLE #TimePeriods
+CREATE TABLE ##TimePeriods
 (
 StartDate	DATE,
 EndDate		DATE
 );
 GO
 
-INSERT INTO #TimePeriods VALUES
+INSERT INTO ##TimePeriods VALUES
 ('1/1/2018','1/5/2018'),
+('1/1/2018','1/3/2018'),
+('1/1/2018','1/2/2018'),
 ('1/3/2018','1/9/2018'),
 ('1/10/2018','1/11/2018'),
 ('1/12/2018','1/16/2018'),
@@ -929,24 +936,21 @@ GO
 WITH cte_TimePeriod_Merge AS
 (
 SELECT	a.StartDate, MIN(b.EndDate) AS EndDate
-FROM	(SELECT	 t1.StartDate
-		 FROM	 #TimePeriods AS t1 LEFT OUTER JOIN
-				 #TimePeriods AS t2 ON t1.StartDate > t2.StartDate AND 
-										t1.StartDate <= t2.StartDate AND 
-										t1.StartDate <= t2.EndDate
-		 GROUP BY t1.StartDate
-		 HAVING COUNT(t2.StartDate) = 0
+FROM	(SELECT	DISTINCT
+				t1.StartDate
+		FROM	##TimePeriods t1
 		) AS a INNER JOIN
-		(SELECT	 t3.EndDate
-		 FROM	 #TimePeriods AS t3 LEFT OUTER JOIN
-				 #TimePeriods AS t4 ON t3.EndDate >= t4.StartDate AND 
+		(
+		SELECT	t3.EndDate
+		FROM	##TimePeriods AS t3 LEFT OUTER JOIN
+				##TimePeriods AS t4 ON t3.EndDate >= t4.StartDate AND
 										t3.EndDate < t4.EndDate
-		 GROUP BY t3.EndDate
-		 HAVING COUNT(t4.StartDate) = 0
+		GROUP BY t3.EndDate
+		HAVING COUNT(t4.StartDate) = 0
 		) AS b ON a.StartDate <= b.EndDate
 GROUP BY a.StartDate
 )
-SELECT	MIN(StartDate) as StartDate, 
+SELECT	MIN(StartDate) as StartDate,
 		MAX(EndDate) as EndDate
 FROM	cte_TimePeriod_Merge
 GROUP BY EndDate;
