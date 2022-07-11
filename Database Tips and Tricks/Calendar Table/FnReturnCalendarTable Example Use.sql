01733484ea92cd2b2317cd34e1fb7233d9d3db12
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2022-07-11 13:12:06 -0500

    Update FnReturnCalendarTable Example Use.sql

--- Database Tips and Tricks/Calendar Table/FnReturnCalendarTable Example Use.sql
+++ Database Tips and Tricks/Calendar Table/FnReturnCalendarTable Example Use.sql
@@ -1,10 +1,22 @@
-/*----------------------------------------
+/*********************************************************************
 Scott Peters
+Creating a Calendar Table
 https://advancedsqlpuzzles.com
-Creating a Calendar Table Example Usage
-*/----------------------------------------
+Last Updated: 07/11/2022
 
-CREATE TABLE CalendarDays
+This script is written in Microsoft SQL Server's T-SQL
+
+This script shows example usage of the function FnReturnCalendarTable.
+See full instructions in PDF format at the following GitHub repository:
+https://github.com/smpetersgithub/AdvancedSQLPuzzles/tree/main/Database%20Tips%20and%20Tricks
+
+**********************************************************************/
+SET NOCOUNT ON;
+DROP TABLE IF EXISTS dbo.CalendarDaysTemp;
+GO
+
+--Create a temporary calendar table
+CREATE TABLE dbo.CalendarDaysTemp
 (
 DateKey INT NOT NULL PRIMARY KEY,
 CalendarDate DATE NOT NULL
@@ -16,27 +28,33 @@ GO
 DECLARE @vStartDate DATE = '2020-01-01';
 WHILE @vStartDate <> '2021-01-01'
     BEGIN
-    INSERT INTO CalendarDays(DateKey, CalendarDate) VALUES
+    INSERT INTO CalendarDaysTemp(DateKey, CalendarDate) VALUES
         (
         CONVERT(INT,CONVERT(VARCHAR, @vStartDate, 112)),
         @vStartDate
         );
+
     SET @vStartDate = DATEADD(DAY,1,@vStartDate);
     END;
 
+-----------------------------------------------------
+-----------------------------------------------------
+
+
 --Basic usage with CROSS APPLY
 SELECT  ct.*
-FROM    CalendarDays cd CROSS APPLY
-        FnReturnCalendarTable(cd.CalendarDate) ct
+FROM    CalendarDaysTemp cd CROSS APPLY
+        dbo.FnReturnCalendarTable(cd.CalendarDate) ct
 WHERE   cd.DateKey = ct.DateKey;
 GO
 
 --Create a view
-ALTER VIEW VwCalendarTable AS
+CREATE OR ALTER VIEW dbo.VwCalendarTable AS
 SELECT  ct.*
-FROM    CalendarDays cd CROSS APPLY
-        FnReturnCalendarTable(cd.CalendarDate) ct
+FROM    CalendarDaysTemp cd CROSS APPLY
+        dbo.FnReturnCalendarTable(cd.CalendarDate) ct
 WHERE   cd.DateKey = ct.DateKey;
 GO
 
-SELECT * FROM VwCalendarTable;
+
+SELECT top 1 * FROM VwCalendarTable;
