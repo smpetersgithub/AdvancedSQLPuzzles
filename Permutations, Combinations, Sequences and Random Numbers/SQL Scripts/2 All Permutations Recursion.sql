8c94eb8aa610738ecefe301279ed4c07028a34b2
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2022-07-19 08:17:14 -0500

    Update 2 All Permutations Recursion.sql

--- Permutations, Combinations, Sequences and Random Numbers/SQL Scripts/2 All Permutations Recursion.sql
+++ Permutations, Combinations, Sequences and Random Numbers/SQL Scripts/2 All Permutations Recursion.sql
@@ -7,10 +7,10 @@ Last Updated: 07/05/2022
 This script is written in SQL Server's T-SQL
 
 
-• Determining permutations can be done with a recursive CTE
-• This solution uses an initial numbers table (#Numbers) that must be populated
-• Output is saved in the temporary table #Permutations
-• Predicate logic is placed on the #Permutations table to limit the output to the correct records
+â€¢ Determining permutations can be done with a recursive CTE
+â€¢ This solution uses an initial numbers table (#Numbers) that must be populated
+â€¢ Output is saved in the temporary table #Permutations
+â€¢ Predicate logic is placed on the #Permutations table to limit the output to the correct records
 
 **********************************************************************/
 
@@ -29,18 +29,18 @@ DECLARE @vTotalNumbers INTEGER = 3;
 ---------------------
 ---------------------
 --Create a #Numbers table using recursion
-WITH cte_Factorial (Number)
+WITH cte_Numbers (Number)
 AS (
     SELECT 1 AS Number
     UNION ALL
     SELECT  Number + 1
-    FROM   cte_Factorial
+    FROM   cte_Numbers
     WHERE  Number < @vTotalNumbers
 )
 SELECT
        Number
 INTO    #Numbers
-FROM   cte_Factorial
+FROM   cte_Numbers
 OPTION (MAXRECURSION 0);--A value of 0 means no limit to the recursion level
 
 ---------------------
