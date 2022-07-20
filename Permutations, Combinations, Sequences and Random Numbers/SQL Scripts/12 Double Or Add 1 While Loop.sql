28ead88713e2a6ceafcd217e8fe533afe5c93509
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2022-07-20 08:23:01 -0500

    Update 12 Double Or Add 1 While Loop.sql

--- Permutations, Combinations, Sequences and Random Numbers/SQL Scripts/12 Double Or Add 1 While Loop.sql
+++ Permutations, Combinations, Sequences and Random Numbers/SQL Scripts/12 Double Or Add 1 While Loop.sql
@@ -7,10 +7,9 @@ Last Updated: 07/05/2022
 This script is written in SQL Server's T-SQL
 
 
-• I can make a safe assumption that less than the desired score (100) numbers are needed
-• To double the number, a WHILE loop is needed
-• To Add 1, we can use windowing
-• The #Numbers table is the only table needed to solve this puzzle
+* I can make a safe assumption that less than the desired score (100) numbers are needed
+* Recursion can be used to solve this problem
+* The #Numbers table is the only table needed to solve this puzzle
 
 **********************************************************************/
 
@@ -37,63 +36,37 @@ AS (
     FROM   cte_Number
     WHERE  Number < @vTotalNumbers
     )
-SELECT Number,
+SELECT CAST(Number AS bigint) AS Number,
        (CASE Number WHEN 1 THEN 1 ELSE NULL END) AS Calculation
 INTO   #Numbers
 FROM   cte_Number
-OPTION (MAXRECURSION 0)--A value of 0 means no limit to the recursion level
+OPTION (MAXRECURSION 101)--A value of 0 means no limit to the recursion level
 
 -------------------------------
 -------------------------------
---Update the #Numbers table with double calcualtion
-WHILE (SELECT MAX(Calculation) * 2 FROM #Numbers) < 100
 
-    BEGIN
-    WITH cte_Min AS
-    (
-    (SELECT MIN(Number) AS MinNumber FROM #Numbers WHERE Calculation IS NULL)
-    ),
-    cte_Lag AS
-    (
-    SELECT Number, LAG(Calculation,1) OVER (ORDER BY Number) * 2 AS LagValue
-    FROM #Numbers
-    ),
-    cte_Numbers AS
-    (
-    SELECT Number, LagValue 
-    FROM   cte_Lag 
-    WHERE Number = (SELECT MinNumber FROM cte_Min)
-    )
-    UPDATE  #Numbers
-    SET     Calculation = b.LagValue
-    FROM    #Numbers a INNER JOIN
-            cte_Numbers b ON a.Number = b.Number
-
-    IF @@ROWCOUNT = 0
-        BEGIN
-        PRINT '@@ROWCOUNT Returned 0.  Increase The Size Of The Numbers Table';
-        RETURN
-        END
-
-    END;
-
--------------------------------
--------------------------------
---Update the #Numbers table with add 1 calculation
-WITH cte_RowNumber AS
+;WITH cte_Numbers AS
 (
-SELECT  ROW_NUMBER() OVER (ORDER BY Number) AS RowNumber2,
-        (SELECT MAX(Calculation) FROM #Numbers) AS MaxNumber,
-        *
+--Add a ranking function here if needed
+--Test data has StepNumber to rank/sort the records.
+SELECT  number
 FROM    #Numbers
-WHERE   Calculation IS NULL
+),
+cte_Recursion AS
+(
+SELECT  Number,
+        CASE WHEN Number = 1 THEN 1 ELSE Number * 2 END AS RunningSum
+FROM    #Numbers
+WHERE   Number = 1
+UNION ALL
+SELECT
+        t.Number,
+        CASE WHEN (RunningSum * 2) < 101 THEN (RunningSum * 2) ELSE RunningSum + 1 END AS RunningSum
+FROM    cte_Recursion cte
+        INNER JOIN
+        cte_Numbers t ON t.Number = (cte.Number + 1)
 )
-UPDATE  #Numbers
-SET     Calculation = MaxNumber + RowNumber2
-FROM    #Numbers a INNER JOIN
-        cte_RowNumber b ON a.Number = b.Number;
-
--------------------------------
--------------------------------
---Display the results
-SELECT * FROM #Numbers WHERE Calculation <= 100;
+SELECT   *
+FROM     cte_Recursion
+WHERE    RunningSum = 100
+ORDER BY Number;
