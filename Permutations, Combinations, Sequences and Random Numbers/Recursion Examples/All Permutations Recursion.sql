b6c64495ea0b7283174377086d50354977c69394
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2022-07-24 17:28:31 -0500

    Add files via upload

---
+++ Permutations, Combinations, Sequences and Random Numbers/Recursion Examples/All Permutations Recursion.sql
@@ -0,0 +1,53 @@
+/*----------------------------------------------------
+Scott Peters
+https://AdvancedSQLPuzzles.com
+
+Displays all permutations for the numbers 1 through n.
+
+*/----------------------------------------------------
+
+---------------------
+---------------------
+DROP TABLE IF EXISTS #Numbers;
+GO
+
+---------------------
+---------------------
+DECLARE @vTotalNumbers INTEGER = 3;
+
+---------------------
+---------------------
+WITH cte_Numbers (Number)
+AS (
+    SELECT 1 AS Number
+    UNION ALL
+    SELECT  Number + 1
+    FROM   cte_Numbers
+    WHERE  Number < @vTotalNumbers
+)
+SELECT
+       Number
+INTO    #Numbers
+FROM   cte_Numbers
+OPTION (MAXRECURSION 0);
+
+---------------------
+---------------------
+WITH cte_Recursion (Permutation, Id, Depth)
+AS
+(
+SELECT  CAST(Number AS VARCHAR(MAX)),
+        CAST(CONCAT(Number,';') AS VARCHAR(MAX)),
+        1 AS Depth
+FROM    #Numbers
+UNION ALL
+SELECT  CONCAT(a.Permutation,',',b.Number),
+        CONCAT(a.Id,b.Number,';'),
+        a.Depth + 1
+FROM    cte_Recursion a,
+        #Numbers b
+WHERE   a.Depth < @vTotalNumbers AND
+        a.Id NOT LIKE CONCAT('%',b.Number,';%')
+)
+SELECT  Permutation
+FROM    cte_Recursion;
---
+++ Permutations, Combinations, Sequences and Random Numbers/Recursion Examples/Managers and Employees Recursion.sql
@@ -0,0 +1,54 @@
+/*----------------------------------------------------
+Scott Peters
+https://AdvancedSQLPuzzles.com
+
+Displays the depth given a managers and employees table.
+
+*/----------------------------------------------------
+
+---------------------
+---------------------
+DROP TABLE IF EXISTS #Employees;
+GO
+
+---------------------
+---------------------
+CREATE TABLE #Employees
+(
+EmployeeID  INTEGER PRIMARY KEY,
+ManagerID   INTEGER,
+JobTitle    VARCHAR(100),
+Salary      INTEGER
+);
+GO
+
+---------------------
+---------------------
+INSERT INTO #Employees VALUES
+(1001,NULL,'President',185000),(2002,1001,'Director',120000),
+(3003,1001,'Office Manager',97000),(4004,2002,'Engineer',110000),
+(5005,2002,'Engineer',142000),(6006,2002,'Engineer',160000);
+GO
+
+---------------------
+---------------------
+WITH cte_Recursion AS
+(
+SELECT  EmployeeID,
+        ManagerID,
+        JobTitle,
+        Salary,
+        0 AS Depth
+FROM    #Employees a
+WHERE   ManagerID IS NULL
+UNION ALL
+SELECT  b.EmployeeID,
+        b.ManagerID,
+        b.JobTitle,
+        b.Salary,
+        a.Depth + 1 AS Depth
+FROM    cte_Recursion a INNER JOIN
+        #Employees b ON a.EmployeeID = b.ManagerID
+)
+SELECT  EmployeeID, ManagerID, JobTitle, Salary, Depth
+FROM    cte_Recursion;
