3753fcaaefdb9532b6076f2af82c6a9adfaf9c8c
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2022-11-17 12:08:12 -0600

    Update Advanced SQL Puzzles Solutions.sql

--- Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
+++ Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
@@ -1799,20 +1799,31 @@ Answer to Puzzle #39
 Prime Numbers
 */----------------------------------------------------
 
-DROP TABLE IF EXISTS #SampleData;
+DROP TABLE IF EXISTS #PrimeNumbers;
 GO
 
-CREATE TABLE #SampleData
+CREATE TABLE #PrimeNumbers
 (
 IntegerValue INTEGER PRIMARY KEY
 );
 GO
 
-INSERT INTO #SampleData VALUES
+INSERT INTO #PrimeNumbers VALUES
 (1),(2),(3),(4),(5),(6),(7),(8),(9),(10);
 GO
 
---Answer coming soon!
+WITH cte_Mod AS
+(
+SELECT  a.IntegerValue, a.IntegerValue % b.IntegerValue AS Modulus
+FROM    #PrimeNumbers a INNER JOIN
+        #PrimeNumbers b ON a.IntegerValue >= b.IntegerValue
+)
+SELECT IntegerValue AS PrimeNumber
+FROM   cte_Mod
+WHERE  Modulus = 0
+GROUP BY IntegerValue
+HAVING COUNT(*) = 2;
+GO
 
 /*----------------------------------------------------
 Answer to Puzzle #40
