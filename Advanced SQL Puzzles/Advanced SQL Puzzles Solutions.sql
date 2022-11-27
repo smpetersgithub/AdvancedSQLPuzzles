47e4e430a7905ed6492f0eb890780ef854e2300b
JadenHoch <82707974+maximilian22x@users.noreply.github.com>
2022-11-27 10:15:33 +0100

    Update Advanced SQL Puzzles Solutions.sql

--- Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
+++ Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
@@ -1441,7 +1441,7 @@ GO
 SELECT  MIN(StepNumber) AS MinStepNumber,
         MAX(StepNumber) as MaxStepNumber,
         [Status],
-        count(*)
+        count(*) as 'Consecutive Count'
 FROM    #Groupings2
 GROUP BY Rnk,
         [Status]
