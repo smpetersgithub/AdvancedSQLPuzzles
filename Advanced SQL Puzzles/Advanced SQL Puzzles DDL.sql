65697c0aafe4ed0ea5be6666b912b47c33320839
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2022-12-10 16:30:46 -0600

    Update Advanced SQL Puzzles DDL.sql

--- Advanced SQL Puzzles/Advanced SQL Puzzles DDL.sql
+++ Advanced SQL Puzzles/Advanced SQL Puzzles DDL.sql
@@ -927,13 +927,13 @@ INSERT INTO #RegionSales VALUES
 ('North','ACE',10),
 ('South','ACE',67),
 ('East','ACE',54),
-('North','Direct Parts',8),
-('South','Direct Parts',7),
-('West','Direct Parts',12),
 ('North','ACME',65),
 ('South','ACME',9),
 ('East','ACME',1),
-('West','ACME',7);
+('West','ACME',7),
+('North','Direct Parts',8),
+('South','Direct Parts',7),
+('West','Direct Parts',12);
 GO
 
 /*----------------------------------------------------
