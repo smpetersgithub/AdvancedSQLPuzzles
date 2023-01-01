939cf6b3e6cdec9bd96e6c674ccb15dea2b70fd5
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-01-01 15:59:46 -0600

    Update Traveling Salesman Recursion.sql

--- Permutations, Combinations, Sequences and Random Numbers/Recursion Examples/Traveling Salesman Recursion.sql
+++ Permutations, Combinations, Sequences and Random Numbers/Recursion Examples/Traveling Salesman Recursion.sql
@@ -18,41 +18,43 @@ GO
 CREATE TABLE #Routes
 (
 FromNode VARCHAR(30) NOT NULL,
-ToNode VARCHAR(30) NOT NULL,
-Distance DECIMAL(28, 12) NOT NULL,
+ToNode   VARCHAR(30) NOT NULL,
+Cost     INTEGER NOT NULL,
 PRIMARY KEY (FromNode, ToNode)
 );
+GO
 
-INSERT #Routes (FromNode, ToNode, Distance)
-OUTPUT inserted.ToNode AS FromNode,
-       inserted.FromNode AS ToNode,
-       inserted.Distance
-INTO   #Routes (FromNode, ToNode, Distance)
+INSERT #Routes (FromNode, ToNode, Cost)
+OUTPUT INSERTED.ToNode AS FromNode,
+       INSERTED.FromNode AS ToNode,
+       INSERTED.Cost
+INTO   #Routes (FromNode, ToNode, Cost)
 VALUES
-('Austin','Dallas', 100),
-('Dallas','Memphis', 200),
-('Memphis','Des Moines', 300),
-('Dallas','Des Moines', 400);
+('Austin','Dallas',100),
+('Dallas','Memphis',200),
+('Memphis','Des Moines',300),
+('Dallas','Des Moines',400);
+GO
 
-
-WITH cteMap(Nodes, LastNode, NodeMap)
+WITH cteMap(Nodes, LastNode, NodeMap, Cost)
 AS (
 SELECT  2 AS Nodes,
         ToNode,
-        CAST('\' + FromNode + '\' + ToNode + '\' AS VARCHAR(MAX)) AS NodeMap
+        CAST('\' + FromNode + '\' + ToNode + '\' AS VARCHAR(MAX)) AS NodeMap,
+        Cost
 FROM    #Routes
+WHERE   FromNode = 'Austin'
 UNION ALL
 SELECT  m.Nodes + 1 AS Nodes,
         r.ToNode AS LastNode,
-        CAST(m.NodeMap + r.ToNode + '\' AS VARCHAR(MAX)) AS NodeMap
+        CAST(m.NodeMap + r.ToNode + '\' AS VARCHAR(MAX)) AS NodeMap,
+		m.Cost + r.Cost AS Cost
 FROM    cteMap AS m INNER JOIN
         #Routes AS r ON r.FromNode = m.LastNode
 WHERE   m.NodeMap NOT LIKE '\%' + r.ToNode + '%\'
 )
-SELECT  NodeMap
+SELECT  NodeMap, Cost
 INTO    #TravelingSalesman
 FROM    cteMap
 OPTION (MAXRECURSION 0);
-
-
-SELECT * FROM #TravelingSalesman;
+GO
