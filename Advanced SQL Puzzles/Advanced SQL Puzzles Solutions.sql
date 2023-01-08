93800cc9c7349a4a304b36255fcad6cc6d2fb545
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-01-08 10:56:29 -0600

    Update Advanced SQL Puzzles Solutions.sql

--- Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
+++ Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
@@ -71,32 +71,30 @@ CREATE TABLE #Employees
 (
 EmployeeID  INTEGER PRIMARY KEY,
 ManagerID   INTEGER,
-JobTitle    VARCHAR(100),
-Salary      INTEGER
+JobTitle    VARCHAR(100)
 );
 GO
 
 INSERT INTO #Employees VALUES
-(1001,NULL,'President',185000),(2002,1001,'Director',120000),
-(3003,1001,'Office Manager',97000),(4004,2002,'Engineer',110000),
-(5005,2002,'Engineer',142000),(6006,2002,'Engineer',160000);
+(1001,NULL,'President'),(2002,1001,'Director'),
+(3003,1001,'Office Manager'),(4004,2002,'Engineer'),
+(5005,2002,'Engineer'),(6006,2002,'Engineer');
 GO
 
 --Recursion
 WITH cte_Recursion AS
 (
-SELECT  EmployeeID, ManagerID, JobTitle, Salary, 0 AS Depth
+SELECT  EmployeeID, ManagerID, JobTitle, 0 AS Depth
 FROM    #Employees a
 WHERE   ManagerID IS NULL
 UNION ALL
-SELECT  b.EmployeeID, b.ManagerID, b.JobTitle, b.Salary, a.Depth + 1 AS Depth
+SELECT  b.EmployeeID, b.ManagerID, b.JobTitle, a.Depth + 1 AS Depth
 FROM    cte_Recursion a INNER JOIN 
         #Employees b ON a.EmployeeID = b.ManagerID
 )
 SELECT  EmployeeID,
         ManagerID,
         JobTitle,
-        Salary,
         Depth
 FROM    cte_Recursion;
 GO
