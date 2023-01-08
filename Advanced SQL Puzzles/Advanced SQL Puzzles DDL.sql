968305ff40dd045ac78e0e18849e0dbc3238ab94
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-01-08 10:52:01 -0600

    Update Advanced SQL Puzzles DDL.sql

--- Advanced SQL Puzzles/Advanced SQL Puzzles DDL.sql
+++ Advanced SQL Puzzles/Advanced SQL Puzzles DDL.sql
@@ -74,7 +74,7 @@ PayRate     MONEY
 GO
 
 /*----------------------------------------------------
-DDL for Puzzle #4
+Answer to Puzzle #4
 Two Predicates
 */----------------------------------------------------
 
@@ -84,7 +84,7 @@ GO
 CREATE TABLE #Orders
 (
 CustomerID     INTEGER,
-OrderID        VARCHAR(100),
+OrderID        INTEGER,
 DeliveryState  VARCHAR(100),
 Amount         MONEY,
 PRIMARY KEY (CustomerID, OrderID)
@@ -92,9 +92,9 @@ PRIMARY KEY (CustomerID, OrderID)
 GO
 
 INSERT INTO #Orders VALUES
-(1001,'Ord936254','CA',340),(1001,'Ord143876','TX',950),(1001,'Ord654876','TX',670),
-(1001,'Ord814356','TX',860),(2002,'Ord342176','WA',320),(3003,'Ord265789','CA',650),
-(3003,'Ord387654','CA',830),(4004,'Ord476126','TX',120);
+(1001,1,'CA',340),(1001,2,'TX',950),(1001,3,'TX',670),
+(1001,4,'TX',860),(2002,5,'WA',320),(3003,6,'CA',650),
+(3003,7,'CA',830),(4004,8,'TX',120);
 GO
 
 /*----------------------------------------------------
