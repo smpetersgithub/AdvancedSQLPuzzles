860b3dbce767a06566b5de4e634369f634f60d7c
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-01-13 13:47:44 -0600

    Update Table Validation Part 1.sql

--- Database Tips and Tricks/Table Validation/Table Validation Part 1.sql
+++ Database Tips and Tricks/Table Validation/Table Validation Part 1.sql
@@ -1,8 +1,12 @@
 /*----------------------------------------------------
 Scott Peters
+Dynamic Full Outer Joins
 https://advancedsqlpuzzles.com
-Last Updated: 10/06/2022
+Last Updated: 01/13/2023
 Microsoft SQL Server T-SQL
+
+This script creates and populates the ##TableInformation
+with the necessary information to peform a dynamic full outer join.
 
 */----------------------------------------------------
 
@@ -19,13 +23,12 @@ Exists1 VARCHAR(1000) NOT NULL,    -- Join condition between Table 1 and Table 2
 Exists2 VARCHAR(1000) NOT NULL);   -- Join condition between Table 1 and Table 2
 GO
 
-
 INSERT INTO ##TableInformation VALUES (
 1---------------LookupID
 ,'dbo'----------Schema1Name
 ,'dbo'----------Schema2Name
-,'MyTable1'-----Table1Name
-,'MyTable2'-----Table2Name
+,'Sales_New'-----Table1Name
+,'Sales_Old'-----Table2Name
 ,'CONCAT(t1.CustID, t1.Region, t1.City)'--Exists1 (this must have the prefix "t1.")
 ,'CONCAT(t2.CustID, t2.Region, t2.City)'--Exists2 (this must have the prefix "t2.")
 );
