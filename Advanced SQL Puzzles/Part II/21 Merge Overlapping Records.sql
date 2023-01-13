f125050f31386e42750a9a1333de0b1a7dae5294
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-01-13 10:28:40 -0600

    Update 21 Merge Overlapping Records.sql

--- Advanced SQL Puzzles/Part II/21 Merge Overlapping Records.sql
+++ Advanced SQL Puzzles/Part II/21 Merge Overlapping Records.sql
@@ -3,8 +3,7 @@ Scott Peters
 Merge Overlapping Records
 https://advancedsqlpuzzles.com
 Last Updated: 01/13/2023
-
-This script is written in SQL Server's T-SQL.
+Microsoft SQL Server T-SQL
 
 This script is used to merge overlapping records. The script creates and populates 
 several temporary tables to aid in the merging process. The script first creates 
