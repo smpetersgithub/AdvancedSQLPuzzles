fb1ab894d40a0578c32c28cfced892fd5a612c16
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-01-13 09:23:51 -0600

    Update 3 Growing Numbers Recursion.sql

--- Advanced SQL Puzzles/Part II/3 Growing Numbers Recursion.sql
+++ Advanced SQL Puzzles/Part II/3 Growing Numbers Recursion.sql
@@ -6,22 +6,30 @@ Last Updated: 01/13/2023
 
 This script is written in SQL Server's T-SQL.
 
-This script is written in SQL Server's T-SQL and is used to find all permutations 
-of a set of integers that are in consecutive order with no gaps. The script creates 
-and uses two temporary tables, #Numbers and #Permutations. The #Numbers table must be 
-manually populated with the integers that the script will use to find permutations. 
-The script then uses a variable to store the total number of integers in the #Numbers table 
-and a recursive common table expression (CTE) to create and populate the #Permutations 
-table with all possible permutations of the integers in the #Numbers table. The CTE creates 
-a permutation by concatenating each integer from the #Numbers table to the previous permutation 
-and only including integers that are one greater than the previous integer. After the CTE is 
-executed, the script uses a SELECT statement with a WHERE clause to filter the #Permutations 
-table and only display permutations that start with the smallest integer in the #Numbers table.
+This script is used to find all possible growing numbers from a set of integers within a specified range. 
+The script creates a temporary table called #GrowingNumbers and uses a recursive CTE to populate it. 
+The script initializes a variable, @vTotalNumbers, with the count of integers in the set.
+
+Given the input 1, 2, 3, 4, and 5.  It will produce the output
+1
+12
+123
+1234
+12345
+
+The CTE, cte_Permutations, is used to generate all possible growing numbers from the set of integers. 
+The CTE starts by selecting the individual integers from the #Numbers table and concatenating them to 
+form a single growing number. Then, it recursively joins itself with the #Numbers table, adding one 
+integer at a time, while ensuring that the next integer added is a successive number.
+
+The script then inserts the results of the CTE into the #GrowingNumbers table, and uses a SELECT 
+statement to display the contents of the table filtered by the condition that the first digit of 
+the number must be equal to the minimum number in the set.
 
 The script will run properly if the following conditions are met:
-1) The first number is a single digit
-2) All digits are successive and there are no gaps in numbers
-3) Two of the numbers cannot be two digits (i.e., 10 and 11)
+The first number is a single digit
+All digits are successive and there are no gaps in numbers
+Two of the numbers cannot be two digits (i.e., 10 and 11)
 
 **********************************************************************/
 
@@ -29,7 +37,7 @@ The script will run properly if the following conditions are met:
 ---------------------
 --Tables used
 DROP TABLE IF EXISTS #Numbers;
-DROP TABLE IF EXISTS #Permutations;
+DROP TABLE IF EXISTS #GrowingNumbers;
 GO
 
 ---------------------
@@ -37,7 +45,7 @@ GO
 --For this puzzle I manually create the #Numbers table to provide special testing cases (rather than using recursion)
 CREATE TABLE #Numbers
 (
-Number INTEGER NOT NULL
+Number INTEGER UNIQUE NOT NULL
 );
 GO
 
@@ -57,15 +65,15 @@ DECLARE @vTotalNumbers INTEGER = (SELECT COUNT(*) FROM #Numbers);
 ---------------------
 ---------------------
 --Create the #Permutations table using recursion
-WITH cte_Permutations (Permutation, Ids, Depth)
+WITH cte_Permutations (Number, Ids, Depth)
 AS
 (
-SELECT  CAST(Number AS VARCHAR(MAX)) AS Permutation,
+SELECT  CAST(Number AS VARCHAR(MAX)) AS Number,
         CAST(CONCAT(Number,'') AS VARCHAR(MAX)) AS Ids,
         1 AS Depth
 FROM    #Numbers
 UNION ALL
-SELECT  CONCAT(a.Permutation,b.Number),
+SELECT  CONCAT(a.Number,b.Number),
         CONCAT(a.Ids,b.Number),
         a.Depth + 1
 FROM    cte_Permutations a,
@@ -73,8 +81,8 @@ FROM    cte_Permutations a,
 WHERE   a.Depth < @vTotalNumbers AND
         CAST(RIGHT(a.Ids,1) AS INTEGER) + 1  = b.Number
 )
-SELECT  Permutation
-INTO    #Permutations
+SELECT  Number
+INTO    #GrowingNumbers
 FROM    cte_Permutations;
 GO
 
@@ -82,6 +90,6 @@ GO
 ---------------------
 --Display the results
 SELECT  *
-FROM    #Permutations
-WHERE   LEFT(Permutation,1) = (SELECT MIN(Number) FROM #Numbers);
+FROM    #GrowingNumbers
+WHERE   LEFT(Number,1) = (SELECT MIN(Number) FROM #Numbers);
 GO
