a6e58c7ee4820b2061773f5927d19b81def05138
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-01-13 10:31:27 -0600

    Update 7 Permutations 1 Through 10 Cross Join.sql

--- Advanced SQL Puzzles/Part II/7 Permutations 1 Through 10 Cross Join.sql
+++ Advanced SQL Puzzles/Part II/7 Permutations 1 Through 10 Cross Join.sql
@@ -3,8 +3,7 @@ Scott Peters
 Permutations 1 Through 10
 https://advancedsqlpuzzles.com
 Last Updated: 01/13/2023
-
-This script is written in SQL Server's T-SQL
+Microsoft SQL Server T-SQL
 
 This script generates permutations of digits from 1 to 10. It uses a table called #Numbers 
 which is populated with the numbers 1 through 10. The script then uses CROSS JOINs to 
@@ -36,6 +35,7 @@ GO
 SELECT  CONCAT(a.Number,',',b.Number) AS Permutation
 FROM    #Numbers a INNER JOIN
         #Numbers b on a.Number <> b.Number;
+GO
 
 --Two digits (Version 2)
 SELECT  CONCAT(a.Number,',',b.Number) AS Permutation
@@ -43,6 +43,7 @@ FROM    #Numbers a CROSS JOIN
         #Numbers b
 WHERE   a.Number NOT IN (b.Number)
 ORDER BY 1;
+GO
 
 -------------------------------
 -------------------------------
@@ -54,6 +55,7 @@ FROM    #Numbers a CROSS JOIN
 WHERE   a.Number NOT IN (b.Number, c.Number) AND
         b.Number NOT IN (c.Number)
 ORDER BY 1;
+GO
 
 -------------------------------
 -------------------------------
@@ -67,6 +69,6 @@ WHERE   a.Number NOT IN (b.Number, c.Number, d.Number) AND
         b.Number NOT IN (c.Number, d.Number) AND
         c.Number NOT IN (d.Number)
 ORDER BY 1;
-
+GO
 
 ----And so on and so forth for 5,6,7.....
