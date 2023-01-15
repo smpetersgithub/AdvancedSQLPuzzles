665f00c9b4b0e32e244781ab8611293524798691
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-01-14 21:00:35 -0600

    Update Advanced SQL Puzzles Solutions.sql

--- Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
+++ Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
@@ -2,7 +2,7 @@
 Scott Peters
 Solutions for Advanced SQL Puzzles
 https://advancedsqlpuzzles.com
-Last Updated: 01/13/2022
+Last Updated: 01/14/2022
 Microsoft SQL Server T-SQL
 
 */----------------------------------------------------
@@ -463,32 +463,34 @@ GO
 INSERT INTO #Employees (EmployeeID, License) VALUES
 (1001,'Class A'),(1001,'Class B'),(1001,'Class C'),
 (2002,'Class A'),(2002,'Class B'),(2002,'Class C'),
-(3003,'Class A'),(3003,'Class D');
+(3003,'Class A'),(3003,'Class D'),
+(4004,'Class A'),(4004,'Class B'),(4004,'Class D'),
+(5005,'Class A'),(5005,'Class B'),(5005,'Class D');
 GO
 
-WITH cte_EmployeeCount AS
+WITH cte_Count AS
 (
 SELECT  EmployeeID,
         COUNT(*) AS LicenseCount
 FROM    #Employees
 GROUP BY EmployeeID
 ),
-cte_EmployeeCountCombined AS
+cte_CountWindow AS
 (
-SELECT  a.EmployeeID AS EmployeeID,
-        b.EmployeeID AS EmployeeID2,
-        COUNT(*) AS LicenseCountCombo
-FROM    #Employees a INNER JOIN
-        #Employees b ON a.License = b.License
-WHERE   a.EmployeeID <> b.EmployeeID
-GROUP BY a.EmployeeID, b.EmployeeID
+SELECT  a.EmployeeID AS EmployeeID_A,
+        b.EmployeeID AS EmployeeID_B,
+        COUNT(*) OVER (PARTITION BY a.EmployeeID, b.EmployeeID) AS CountWindow
+FROM    #Employees a CROSS JOIN
+        #Employees b
+WHERE   a.EmployeeID <> b.EmployeeID and a.License = b.License
 )
-SELECT  a.EmployeeID,
-        a.EmployeeID2,
-        a.LicenseCountCombo
-FROM    cte_EmployeeCountCombined a INNER JOIN
-        cte_EmployeeCount b ON  a.LicenseCountCombo = b.LicenseCount AND
-                                a.EmployeeID <> b.EmployeeID;
+SELECT  DISTINCT
+        a.EmployeeID_A,
+        a.EmployeeID_B,
+        a.CountWindow AS LicenseCount
+FROM    cte_CountWindow a INNER JOIN
+        cte_Count b ON a.CountWindow = b.LicenseCount and a.EmployeeID_A = b.EmployeeID INNER JOIN
+        cte_Count c ON a.CountWindow = c.LicenseCount and a.EmployeeID_B = c.EmployeeID;
 GO
 
 /*----------------------------------------------------
