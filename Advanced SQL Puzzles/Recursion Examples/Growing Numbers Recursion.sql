1a2b904f350f7cf92cd18b52827aa33120e766b6
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-01-27 14:02:27 -0600

    Update Growing Numbers Recursion.sql

--- Advanced SQL Puzzles/Recursion Examples/Growing Numbers Recursion.sql
+++ Advanced SQL Puzzles/Recursion Examples/Growing Numbers Recursion.sql
@@ -2,7 +2,7 @@
 Scott Peters
 Growing Numbers
 https://advancedsqlpuzzles.com
-Last Updated: 01/13/2022
+Last Updated: 01/13/2023
 Microsoft SQL Server T-SQL
 
 This script uses recursion to create a growing number list.
