d44efea1a9ba859c9145b24e437be7600bf92acd
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-10-05 12:07:51 -0500

    Update 03 Growing Numbers Recursion.sql

--- Advanced SQL Puzzles/Part II/03 Growing Numbers Recursion.sql
+++ Advanced SQL Puzzles/Part II/03 Growing Numbers Recursion.sql
@@ -5,8 +5,8 @@ https://advancedsqlpuzzles.com
 Last Updated: 01/13/2023
 Microsoft SQL Server T-SQL
 
-This script is used to find all possible growing numbers from a set of integers within a specified range. 
-For example, given the input 1, 2, 3, 4, and 5.  It will produce the output:
+This script finds all possible growing numbers from a set of integers within a specified range. 
+For example, given the input 1, 2, 3, 4, and 5.  It will produce the following output:
 1
 12
 123
@@ -15,18 +15,18 @@ For example, given the input 1, 2, 3, 4, and 5.  It will produce the output:
 
 The script creates a temporary table called #GrowingNumbers and uses a recursive CTE to populate it. 
 The script initializes a variable, @vTotalNumbers, with the count of integers in the set.
-The CTE, cte_GrowingNumbers, is used to generate all possible growing numbers from the set of integers. 
+The CTE, cte_GrowingNumbers, generates all possible growing numbers from the set of integers. 
 The CTE starts by selecting the individual integers from the #Numbers table and concatenating them to 
 form a single growing number. Then, it recursively joins itself with the #Numbers table, adding one 
-integer at a time, while ensuring that the next integer added is a successive number.
+integer at a time while ensuring that the next integer added is a successive number.
 
-The script then inserts the results of the CTE into the #GrowingNumbers table, and uses a SELECT 
+The script then inserts the results of the CTE into the #GrowingNumbers table and uses a SELECT 
 statement to display the contents of the table filtered by the condition that the first digit of 
 the number must be equal to the minimum number in the set.
 
 The script will run properly if the following conditions are met:
-The first number is a single digit
-All digits are successive and there are no gaps in numbers
+The first number is a single-digit
+All digits are successive, and there are no gaps in the numbers
 Two of the numbers cannot be two digits (i.e., 10 and 11)
 
 **********************************************************************/
@@ -40,7 +40,7 @@ GO
 
 ---------------------
 ---------------------
---For this puzzle I manually create the #Numbers table to provide special testing cases (rather than using recursion)
+--For this puzzle, I manually create the #Numbers table to provide special testing cases (rather than using recursion)
 CREATE TABLE #Numbers
 (
 Number INTEGER UNIQUE NOT NULL
@@ -52,7 +52,7 @@ INSERT INTO #Numbers (Number) VALUES
 --(6),(7),(8),(9),(10); --Passes
 --(5),(7),(8),(9),(10); --Does Not Pass (Gap between 5 and 7)
 --(7),(8),(9),(10),(11); --Does Not Pass (Two numbers greater than 10)
---(10),(11),(12); --Does Not Pass (Lowest number is not a single digit and the set contains two numbers greater than 10)
+--(10),(11),(12); --Does Not Pass (The lowest number is not a single digit, and the set contains two numbers greater than 10)
 GO
 
 ---------------------
