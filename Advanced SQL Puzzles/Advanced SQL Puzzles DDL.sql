0a3cfe4520c09652398276492e538a156373b3f5
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2023-12-18 11:09:39 -0600

    Update Advanced SQL Puzzles DDL.sql

--- Advanced SQL Puzzles/Advanced SQL Puzzles DDL.sql
+++ Advanced SQL Puzzles/Advanced SQL Puzzles DDL.sql
@@ -1601,5 +1601,32 @@ INSERT INTO #Parts (SerialNumber, ManufactureDay, Product) VALUES
 GO
 
 /*----------------------------------------------------
+DDL for Puzzle #67
+Matching Birthdays
+*/----------------------------------------------------
+
+DROP TABLE IF EXISTS #Students;
+GO
+
+CREATE TABLE #Students
+(
+StudentName VARCHAR(50) PRIMARY KEY,
+Birthday    DATE
+);
+GO
+
+INSERT INTO #Students (StudentName, Birthday) VALUES 
+('Susan', '2015-04-15'),
+('Tim', '2015-04-15'),
+('Jacob', '2015-04-15'),
+('Earl', '2015-02-05'),
+('Mike', '2015-05-23'),
+('Angie', '2015-05-23'),
+('Jenny', '2015-11-19'),
+('Michelle', '2015-12-12'),
+('Aaron', '2015-12-18');
+GO
+
+/*----------------------------------------------------
 The End
 */----------------------------------------------------
