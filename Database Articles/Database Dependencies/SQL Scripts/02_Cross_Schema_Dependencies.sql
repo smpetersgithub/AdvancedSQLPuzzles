d64157ba48c799eb70e9e91860d4dbafdc989cc8
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2024-12-05 09:13:34 -0600

    Add files via upload

--- Database Articles/Database Dependencies/SQL Scripts/02_Cross_Schema_Dependencies.sql
+++ Database Articles/Database Dependencies/SQL Scripts/02_Cross_Schema_Dependencies.sql
@@ -43,7 +43,7 @@ GO
 USE foo;
 GO
 
-DECLARE @vTruncate SMALLINT = 1;
+DECLARE @vTruncate SMALLINT = 0;
 IF @vTruncate = 1
 BEGIN
      TRUNCATE TABLE foo.dbo.sql_expression_dependencies;
@@ -54,11 +54,18 @@ USE foo;
 GO
 
 INSERT INTO foo.dbo.sql_expression_dependencies
-(database_name, example_number, referencing_object_type, referencing_entity_name, referencing_id, referencing_minor_id, referencing_class, referencing_class_desc, is_schema_bound_reference, referenced_class, referenced_class_desc, referenced_server_name, referenced_database_name, referenced_schema_name, referenced_entity_name, referenced_object_type, referenced_id, referenced_minor_id, is_caller_dependent, is_ambiguous)
-SELECT  'foo', '02', c.type AS referencing_object_type, c.name AS referencing_entity_name, referencing_id, referencing_minor_id, referencing_class, referencing_class_desc, is_schema_bound_reference, referenced_class, referenced_class_desc, referenced_server_name, referenced_database_name, referenced_schema_name, referenced_entity_name, b.type AS referenced_object_type, referenced_id, referenced_minor_id, is_caller_dependent, is_ambiguous
+(example_number, referencing_object_type, referencing_server_name, referencing_database_name, referencing_schema_name, referencing_entity_name, referencing_id, referencing_minor_id, referencing_class, referencing_class_desc, is_schema_bound_reference, referenced_class, referenced_class_desc, referenced_server_name, referenced_database_name, referenced_schema_name, referenced_entity_name, referenced_object_type, referenced_id, referenced_minor_id, is_caller_dependent, is_ambiguous)
+SELECT  '02', COALESCE(c.type, d.type, e.type) AS referencing_object_type, @@SERVERNAME, DB_NAME(), SCHEMA_NAME(c.schema_id), COALESCE(c.name, d.name, e.name) AS referencing_entity_name, referencing_id, referencing_minor_id, referencing_class, referencing_class_desc, is_schema_bound_reference, referenced_class, referenced_class_desc, referenced_server_name, referenced_database_name, referenced_schema_name, referenced_entity_name, b.type AS referenced_object_type, referenced_id, referenced_minor_id, is_caller_dependent, is_ambiguous
 FROM    sys.sql_expression_dependencies a LEFT OUTER JOIN
         sys.objects b ON a.referenced_id = b.object_id LEFT OUTER JOIN
-        sys.objects c ON a.referencing_id = c.object_id;
+        sys.objects c ON a.referencing_id = c.object_id LEFT OUTER JOIN
+        sys.triggers d ON a.referencing_id = d.object_id LEFT OUTER JOIN
+        sys.server_triggers e ON a.referencing_id = e.object_id;
+GO
+
+INSERT INTO foo.dbo.system_objects (table_name, object_id, server_name, database_name, schema_name, name, principal_id, schema_id, parent_object_id, type, type_desc, create_date, modify_date, is_ms_shipped, is_published, is_schema_published) 
+SELECT 'sys.objects', object_id, @@SERVERNAME, DB_NAME(), SCHEMA_NAME(schema_id), name, principal_id, schema_id, parent_object_id, type, type_desc, create_date, modify_date, is_ms_shipped, is_published, is_schema_published 
+FROM   sys.objects
 GO
 
 -------------------------------------------------------
--- Database Articles/Database Dependencies/SQL Scripts/19_Contracts_and_Queues_and_Message_Types.sql
+++ Database Articles/Database Dependencies/SQL Scripts/19_Contracts_and_Queues_and_Message_Types.sql
@@ -1,3 +1,11 @@
+/*
+
+Before executing this script, ensure all connections are disconnected!!
+Before executing this script, ensure all connections are disconnected!!
+Before executing this script, ensure all connections are disconnected!!
+
+*/
+
 ------------------------
 USE foo;
 GO
@@ -76,7 +84,7 @@ BEGIN DIALOG CONVERSATION @DialogHandle
 -- Send a message to the queue as NVARCHAR to ensure proper encoding
 SEND ON CONVERSATION @DialogHandle
     MESSAGE TYPE msg_type_example_19
-    (N'Hello, this is a test message!');  -- Notice the N prefix for NVARCHAR
+    (N'Hello, this is a test message!');
 GO
 
 ---------------------------------
@@ -92,22 +100,30 @@ GO
 USE foo;
 GO
 
-DECLARE @vTruncate SMALLINT = 1;
+DECLARE @vTruncate SMALLINT = 0;
 IF @vTruncate = 1
 BEGIN
      TRUNCATE TABLE foo.dbo.sql_expression_dependencies;
 END;
 GO
+
 -------------------------------------------------------
 USE foo;
 GO
 
 INSERT INTO foo.dbo.sql_expression_dependencies
-(database_name, example_number, referencing_object_type, referencing_entity_name, referencing_id, referencing_minor_id, referencing_class, referencing_class_desc, is_schema_bound_reference, referenced_class, referenced_class_desc, referenced_server_name, referenced_database_name, referenced_schema_name, referenced_entity_name, referenced_object_type, referenced_id, referenced_minor_id, is_caller_dependent, is_ambiguous)
-SELECT  'foo', '19', c.type AS referencing_object_type, c.name AS referencing_entity_name, referencing_id, referencing_minor_id, referencing_class, referencing_class_desc, is_schema_bound_reference, referenced_class, referenced_class_desc, referenced_server_name, referenced_database_name, referenced_schema_name, referenced_entity_name, b.type AS referenced_object_type, referenced_id, referenced_minor_id, is_caller_dependent, is_ambiguous
+(example_number, referencing_object_type, referencing_server_name, referencing_database_name, referencing_schema_name, referencing_entity_name, referencing_id, referencing_minor_id, referencing_class, referencing_class_desc, is_schema_bound_reference, referenced_class, referenced_class_desc, referenced_server_name, referenced_database_name, referenced_schema_name, referenced_entity_name, referenced_object_type, referenced_id, referenced_minor_id, is_caller_dependent, is_ambiguous)
+SELECT  '19', COALESCE(c.type, d.type, e.type) AS referencing_object_type, @@SERVERNAME, DB_NAME(), SCHEMA_NAME(c.schema_id), COALESCE(c.name, d.name, e.name) AS referencing_entity_name, referencing_id, referencing_minor_id, referencing_class, referencing_class_desc, is_schema_bound_reference, referenced_class, referenced_class_desc, referenced_server_name, referenced_database_name, referenced_schema_name, referenced_entity_name, b.type AS referenced_object_type, referenced_id, referenced_minor_id, is_caller_dependent, is_ambiguous
 FROM    sys.sql_expression_dependencies a LEFT OUTER JOIN
         sys.objects b ON a.referenced_id = b.object_id LEFT OUTER JOIN
-        sys.objects c ON a.referencing_id = c.object_id;
+        sys.objects c ON a.referencing_id = c.object_id LEFT OUTER JOIN
+        sys.triggers d ON a.referencing_id = d.object_id LEFT OUTER JOIN
+        sys.server_triggers e ON a.referencing_id = e.object_id;
+GO
+
+INSERT INTO foo.dbo.system_objects (table_name, object_id, server_name, database_name, schema_name, name, principal_id, schema_id, parent_object_id, type, type_desc, create_date, modify_date, is_ms_shipped, is_published, is_schema_published) 
+SELECT 'sys.objects', object_id, @@SERVERNAME, DB_NAME(), SCHEMA_NAME(schema_id), name, principal_id, schema_id, parent_object_id, type, type_desc, create_date, modify_date, is_ms_shipped, is_published, is_schema_published 
+FROM   sys.objects
 GO
 
 -------------------------------------------------------
@@ -117,7 +133,6 @@ GO
 --------------------------
 USE foo;
 GO
-
 
 --drop service
 DECLARE @vDropObjects SMALLINT = 1;
