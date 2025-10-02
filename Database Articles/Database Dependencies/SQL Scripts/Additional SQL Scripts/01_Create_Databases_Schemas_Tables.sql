a79610bd537a52972d2637224bb138d49c0bd8f6
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2025-10-02 10:15:21 -0500

    Update 01_Create_Databases_Schemas_Tables.sql

--- Database Articles/Database Dependencies/SQL Scripts/Additional SQL Scripts/01_Create_Databases_Schemas_Tables.sql
+++ Database Articles/Database Dependencies/SQL Scripts/Additional SQL Scripts/01_Create_Databases_Schemas_Tables.sql
@@ -63,29 +63,3 @@ is_ambiguous INT NOT NULL
 );
 GO
 
-----------------------------------------
-USE foo;
-GO
-
-DROP TABLE IF EXISTS dbo.system_objects;
-GO
-
-CREATE TABLE dbo.system_objects (
-table_name NVARCHAR(128) NULL,
-object_id INT NOT NULL,
-server_name NVARCHAR(128) NULL,
-database_name NVARCHAR(128) NOT NULL,
-schema_name NVARCHAR(128) NULL,
-name NVARCHAR(128) NULL,
-principal_id INT NULL,
-schema_id INT NULL,
-parent_object_id INT NULL,
-type VARCHAR(200) NULL,
-type_desc NVARCHAR(60) NULL,
-create_date DATETIME NULL,
-modify_date DATETIME NULL,
-is_ms_shipped BIT NULL,
-is_published BIT NULL,
-is_schema_published BIT NULL,
-CONSTRAINT PK_sys_objects PRIMARY KEY (object_id, database_name) WITH (IGNORE_DUP_KEY = ON)
-);
