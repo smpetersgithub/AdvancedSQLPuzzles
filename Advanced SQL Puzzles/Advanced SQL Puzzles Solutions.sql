9ba77fe32d9ea9ecc6f06cb4aa78bd9d293dd540
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2025-12-18 15:15:46 -0600

    Update Advanced SQL Puzzles Solutions.sql

--- Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
+++ Advanced SQL Puzzles/Advanced SQL Puzzles Solutions.sql
@@ -416,8 +416,8 @@ GO
 
 INSERT INTO #Candidates (CandidateID, Occupation) VALUES
 (1001,'Geologist'),(1001,'Astrogator'),(1001,'Biochemist'),
-(1001,'Technician'),(2002,'Surgeon'),(2002,'Machinist'),
-(3003,'Cryologist'),(4004,'Selenologist');
+(1001,'Technician'),(2002,'Surgeon'),(2002,'Machinist'),(2002,'Geologist'),
+(3003,'Geologist'),(3003,'Astrogrator'),(4004,'Selenologist');
 GO
 
 CREATE TABLE #Requirements
@@ -4186,3 +4186,4 @@ GO
 /*----------------------------------------------------
 The End
 */----------------------------------------------------
+
