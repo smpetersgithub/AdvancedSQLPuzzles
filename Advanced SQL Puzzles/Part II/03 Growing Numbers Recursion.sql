c27cb54bb54d92287ae55311c1bb3c904b710395
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2025-12-18 18:44:46 -0600

    Update 03 Growing Numbers Recursion.sql

--- Advanced SQL Puzzles/Part II/03 Growing Numbers Recursion.sql
+++ Advanced SQL Puzzles/Part II/03 Growing Numbers Recursion.sql
@@ -4,31 +4,6 @@ Growing Numbers
 https://advancedsqlpuzzles.com
 Last Updated: 01/13/2023
 Microsoft SQL Server T-SQL
-
-This script finds all possible growing numbers from a set of integers within a specified range. 
-For example, given the input 1, 2, 3, 4, and 5.  It will produce the following output:
-1
-12
-123
-1234
-12345
-
-The script creates a temporary table called #GrowingNumbers and uses a recursive CTE to populate it. 
-The script initializes a variable, @vTotalNumbers, with the count of integers in the set.
-The CTE, cte_GrowingNumbers, generates all possible growing numbers from the set of integers. 
-The CTE starts by selecting the individual integers from the #Numbers table and concatenating them to 
-form a single growing number. Then, it recursively joins itself with the #Numbers table, adding one 
-integer at a time while ensuring that the next integer added is a successive number.
-
-The script then inserts the results of the CTE into the #GrowingNumbers table and uses a SELECT 
-statement to display the contents of the table filtered by the condition that the first digit of 
-the number must be equal to the minimum number in the set.
-
-The script will run properly if the following conditions are met:
-The first number is a single-digit
-All digits are successive, and there are no gaps in the numbers
-Two of the numbers cannot be two digits (i.e., 10 and 11)
-
 **********************************************************************/
 
 ---------------------
@@ -91,3 +66,4 @@ SELECT  *
 FROM    #GrowingNumbers
 WHERE   LEFT(Number,1) = (SELECT MIN(Number) FROM #Numbers);
 GO
+
