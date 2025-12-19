f069333dca078321b98e9bdcc75002e03645fa56
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2025-12-18 18:52:34 -0600

    Update 10 Seating Chart.sql

--- Advanced SQL Puzzles/Part II/10 Seating Chart.sql
+++ Advanced SQL Puzzles/Part II/10 Seating Chart.sql
@@ -4,16 +4,6 @@ Seating Chart
 https://advancedsqlpuzzles.com
 Last Updated: 01/13/2023
 Microsoft SQL Server T-SQL
-
-The script then uses several SQL statements to analyze the data in the #SeatingChart table. 
-The first query uses the LEAD() window function to find the gaps in the seat numbers and 
-returns the gap start and gap end values. 
-
-The second query uses a common table expression (CTE) and the ROW_NUMBER() function to find 
-the missing seat numbers by ranking them and calculating the difference 
-between each seat number and its rank. The last query uses the COUNT() function and 
-a CASE statement to determine the number of odd and even seat numbers in the #SeatingChart table.
-
 **********************************************************************/
 
 --------------
@@ -54,7 +44,8 @@ FROM
     FROM #SeatingChart
     ) a
 WHERE Gap > 1;
-
+GO
+        
 --Missing Numbers
 WITH cte_Rank
 AS
@@ -66,9 +57,11 @@ FROM    #SeatingChart
 WHERE   SeatNumber > 0
 )
 SELECT MAX(Rnk) AS MissingNumbers FROM cte_Rank;
+GO
 
 --Odd and even number count
 SELECT  (CASE SeatNumber%2 WHEN 1 THEN 'Odd' WHEN 0 THEN 'Even' END) AS Modulus,
         COUNT(*) AS [Count]
 FROM    #SeatingChart
 GROUP BY (CASE SeatNumber%2 WHEN 1 THEN 'Odd' WHEN 0 THEN 'Even' END);
+GO
