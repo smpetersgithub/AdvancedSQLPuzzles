234c7b8e14f63b1e27c17df7b4f6a46a25eeaf7a
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2025-12-18 18:50:25 -0600

    Update 09 Find The Spaces Recursion.sql

--- Advanced SQL Puzzles/Part II/09 Find The Spaces Recursion.sql
+++ Advanced SQL Puzzles/Part II/09 Find The Spaces Recursion.sql
@@ -4,16 +4,6 @@ Find The Spaces
 https://advancedsqlpuzzles.com
 Last Updated: 02/07/2023
 Microsoft SQL Server T-SQL
-
-This script uses recursion to find the spaces in a given string and determine their start and position points. 
-The script starts by creating a table called #Strings that stores a set of strings.  The script then uses a common 
-table expression (CTE) called cte_CAST to cast the strings in the #Strings table to VARCHAR(200) and a CTE called 
-cte_Anchor to find the spaces in the strings recursively. The cte_Anchor CTE uses the CHARINDEX function to find 
-the position of the space character in the string and the Position column to find the next space in the
-string recursively. The script also uses the SUBSTRING function to extract the word between spaces and the LEN function and REPLACE 
-function to find the total number of spaces in the string. The script then uses the ROW_NUMBER() function to assign 
-a unique row number to each space found and displays the results.
-
 **********************************************************************/
 
 -------------------------------
