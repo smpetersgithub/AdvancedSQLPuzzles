c11b36c10c9bb2aa7b91f33fcbbd60e2d7763f57
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2025-12-18 18:47:56 -0600

    Update 06 Permutations Of 0 And 1 While Loop.sql

--- Advanced SQL Puzzles/Part II/06 Permutations Of 0 And 1 While Loop.sql
+++ Advanced SQL Puzzles/Part II/06 Permutations Of 0 And 1 While Loop.sql
@@ -4,17 +4,6 @@ Permutations of 0 and 1
 https://advancedsqlpuzzles.com
 Last Updated: 01/13/2023
 Microsoft SQL Server T-SQL
-
-This script creates a temporary table #Permutations and inserts 0 and 1 as initial values. 
-It then declares a variable @vPermutationLength, which is used to set the length of the permutation string.
-Then, using a while loop, it repeatedly concatenates the current permutations in the table 
-to create new permutations until the maximum length of the permutation column in the table 
-equals the value of @vPermutationLength. Finally, it selects and displays distinct permutations 
-of the desired length from the #Permutations table, ordered by the permutation values.
-
-Given the input of 0 and 1 and a @vPermutationLength of 3, it will produce the following output:
-000, 001, 010, 011, 100, 101, 110, 111
-
 **********************************************************************/
 
 -------------------------------
