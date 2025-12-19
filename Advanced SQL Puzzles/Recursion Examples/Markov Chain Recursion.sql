f38032851361a3332437d77e988afcd6c38adb86
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2025-12-18 19:15:11 -0600

    Update Markov Chain Recursion.sql

--- Advanced SQL Puzzles/Recursion Examples/Markov Chain Recursion.sql
+++ Advanced SQL Puzzles/Recursion Examples/Markov Chain Recursion.sql
@@ -5,14 +5,7 @@ https://advancedsqlpuzzles.com
 Last Updated: 02/07/2023
 Microsoft SQL Server T-SQL
 
-This script uses recursion to solve a Markov Chain.
 https://en.wikipedia.org/wiki/Markov_chain
-
-In Probability Land, on a sunny day, there is an equal probability of the next day being sunny or rainy. 
-On a rainy day, there is a 70% chance it will rain the next day and a 30% chance it will be sunny the next day.
-
-On average, how many rainy days are there in Probability Land?
-
 */----------------------------------------------------
 
 -------------------------------
