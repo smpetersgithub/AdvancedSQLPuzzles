299960dddbebe814cb66d3799a01dfec5b7e326d
Scott Peters <56611711+smpetersgithub@users.noreply.github.com>
2026-08-24 19:44:43 -0500

    Update 31_XML_Schema_Collections.sql

--- Database Articles/Database Dependencies/SQL Scripts/31_XML_Schema_Collections.sql
+++ Database Articles/Database Dependencies/SQL Scripts/31_XML_Schema_Collections.sql
@@ -69,8 +69,9 @@ GO
 
 INSERT INTO foo.dbo.sql_expression_dependencies
 (example_number, referencing_object_type, referencing_server_name, referencing_database_name, referencing_schema_name, referencing_entity_name, referencing_id, referencing_minor_id, referencing_class, referencing_class_desc, is_schema_bound_reference, referenced_class, referenced_class_desc, referenced_server_name, referenced_database_name, referenced_schema_name, referenced_entity_name, referenced_object_type, referenced_id, referenced_minor_id, is_caller_dependent, is_ambiguous, referencing_is_ms_shipped, referenced_is_ms_shipped, is_self_referencing)
-SELECT  '31', c.type, @@SERVERNAME, DB_NAME(), SCHEMA_NAME(c.schema_id), c.name, a.referencing_id, a.referencing_minor_id, a.referencing_class, a.referencing_class_desc, a.is_schema_bound_reference, a.referenced_class, a.referenced_class_desc, a.referenced_server_name, a.referenced_database_name, a.referenced_schema_name, a.referenced_entity_name, 'XML Schema Collection', a.referenced_id, a.referenced_minor_id, a.is_caller_dependent, a.is_ambiguous, c.is_ms_shipped, NULL, CASE WHEN a.referencing_id = a.referenced_id THEN 1 ELSE 0 END
+SELECT  '31', c.type, @@SERVERNAME, DB_NAME(), SCHEMA_NAME(c.schema_id), c.name, a.referencing_id, a.referencing_minor_id, a.referencing_class, a.referencing_class_desc, a.is_schema_bound_reference, a.referenced_class, a.referenced_class_desc, a.referenced_server_name, a.referenced_database_name, a.referenced_schema_name, a.referenced_entity_name, ISNULL(CAST(b.type AS VARCHAR(100)), 'XML'), a.referenced_id, a.referenced_minor_id, a.is_caller_dependent, a.is_ambiguous, c.is_ms_shipped, NULL, CASE WHEN a.referencing_id = a.referenced_id THEN 1 ELSE 0 END
 FROM    sys.sql_expression_dependencies a LEFT OUTER JOIN
+        sys.objects b ON a.referenced_id = b.object_id LEFT OUTER JOIN
         sys.objects c ON a.referencing_id = c.object_id LEFT OUTER JOIN
         sys.xml_schema_collections f ON a.referenced_id = f.xml_collection_id;
 GO
